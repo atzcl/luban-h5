a13007e504f711d7c596794506f460626ffabcff
ly525 <lantern.done@gmail.com>
2019-07-15 00:39:06 +0800

    deploy

---
+++ precache-manifest.f70cf7681ec3006fc391f3a9239ac6ae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/luban-h5/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/luban-h5/robots.txt"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/luban-h5/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "360c2f6c82459101c25e",
+    "url": "/luban-h5/js/chunk-2d22d746.2d4bda30.js"
+  },
+  {
+    "revision": "3eca44fee1ca34957e3e",
+    "url": "/luban-h5/js/chunk-8e74d4e0.1bbede7b.js"
+  },
+  {
+    "revision": "25756a20ce58d14585ac",
+    "url": "/luban-h5/js/chunk-vendors.02d1a361.js"
+  },
+  {
+    "revision": "c8ac69ba73a81f871d60",
+    "url": "/luban-h5/js/app.3c08c8f5.js"
+  },
+  {
+    "revision": "aae56cac632a05f2d816c286fa61d610",
+    "url": "/luban-h5/index.html"
+  },
+  {
+    "revision": "f83f398859adf0e150f4154222dff84c",
+    "url": "/luban-h5/img/lbp-picture-placeholder.f83f3988.jpg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/luban-h5/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/luban-h5/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/luban-h5/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "25756a20ce58d14585ac",
+    "url": "/luban-h5/css/chunk-vendors.732cfde8.css"
+  },
+  {
+    "revision": "3eca44fee1ca34957e3e",
+    "url": "/luban-h5/css/chunk-8e74d4e0.19820973.css"
+  },
+  {
+    "revision": "c8ac69ba73a81f871d60",
+    "url": "/luban-h5/css/app.9041fe2e.css"
+  }
+];
